55f2f3f22657c51a3de2fea3fb5499fb6930f5da
tnscks <114800778+tnscks@users.noreply.github.com>
2023-11-01 11:23:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -69,18 +69,6 @@ st.code('x=1234')
 st.markdown('## Code & Latex')
 st.latex(r''' a+ar+ar^2+ae^3''')
 
-
-
-# write 연습하기
-st.title('write')
-st.caption('참고사이트: https://docs.streamlit.io/library/api-reference/write-magic/st.write')
-st.text('아래 딕셔너리를 판다스 데이터프레임으로 변경')
-st.caption("{'이름': ['홍길동', '김사랑', '일지매', '이루리'],'수준': ['금', '동', '은', '은']}")
-df = pd.DataFrame({'이름':['홍길동', '김사랑', '일지매', '이루리'], '수준':['금', '동', '은', '은']} )
-st.write(df, '스트림릿의 write 함수로 표현')
-
-# 파일실행: File > New > Terminal(anaconda prompt) - streamlit run streamlit\1.text.py
-
 st.markdown("지도 2")
 
 html = """<!DOCTYPE html>

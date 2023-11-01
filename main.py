e5e9638fc586a5b575a87e4de506c69c21ae3e2a
tnscks <114800778+tnscks@users.noreply.github.com>
2023-11-01 10:43:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,52 +40,6 @@ st.write(df, '스트림릿의 write 함수로 표현')
 
 # 파일실행: File > New > Terminal(anaconda prompt) - streamlit run streamlit\1.text.py
 
-var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-        style: naver.maps.MapTypeControlStyle.DROPDOWN
-    }
-});
-
-var trafficLayer = new naver.maps.TrafficLayer({
-    interval: 300000 // 5분마다 새로고침 (최소값 5분)
-});
-
-var btn = $('#traffic');
-
-naver.maps.Event.addListener(map, 'trafficLayer_changed', function(trafficLayer) {
-    if (trafficLayer) {
-        btn.addClass('control-on');
-        $("#autorefresh").parent().show();
-        $("#autorefresh")[0].checked = true;
-    } else {
-        btn.removeClass('control-on');
-        $("#autorefresh").parent().hide();
-    }
-});
-
-btn.on("click", function(e) {
-    e.preventDefault();
-
-    if (trafficLayer.getMap()) {
-        trafficLayer.setMap(null);
-    } else {
-        trafficLayer.setMap(map);
-    }
-});
-
-$("#autorefresh").on("click", function(e) {
-    var btn = $(this),
-        checked = btn.is(":checked");
-
-    if (checked) {
-        trafficLayer.startAutoRefresh();
-    } else {
-        trafficLayer.endAutoRefresh();
-    }
-});
-
-naver.maps.Event.once(map, 'init', function() {
-    trafficLayer.setMap(map);
-});
+with open("script.js", "r", encoding="utf-8") as js_file:
+    js_code = js_file.read()
+components.html(js_code, height=800, width=800)

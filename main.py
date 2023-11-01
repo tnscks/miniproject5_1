331ae6d15abd5bf30780d539592cc52db029f86a
tnscks <114800778+tnscks@users.noreply.github.com>
2023-11-01 11:23:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -86,21 +86,8 @@ st.markdown("지도 2")
 html = """<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
-    <title>네이버 기본지도</title>
-    <script type="text/javascript" src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=61n21o4wj8"></script>
-</head>
-<body>
-<div id="map" style="width:100%;height:400px;"></div>
-<script>
-var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
-};
 
-var map = new naver.maps.Map('map', mapOptions);
-</script>
+<title>네이버 기본지도</title>
+
 </body>
 </html> """

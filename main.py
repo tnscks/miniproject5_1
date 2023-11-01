58cfb549cd8ff10d7f3d00bf92a2078a4bbfd907
tnscks <114800778+tnscks@users.noreply.github.com>
2023-11-01 11:38:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+pip install git+https://github.com/PiotrDabkowski/Js2Py
 # streamlit, pandas 라이브러리 불러오기 
 import streamlit as st
 import pandas as pd
@@ -72,10 +73,28 @@ st.latex(r''' a+ar+ar^2+ae^3''')
 st.markdown("지도 2")
 
 html = """<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <title>간단한 지도 표시하기</title>
+    <script type="text/javascript" src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=YOUR_CLIENT_ID"></script>
+</head>
+<body>
+<div id="map" style="width:100%;height:400px;"></div>
 
-<title>네이버 기본지도</title>
+<script>
+var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+};
+
+var map = new naver.maps.Map('map', mapOptions);
+</script>
+</body>
+</html>
 
 </body>
 </html> """
